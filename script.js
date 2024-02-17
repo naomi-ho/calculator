29383632e9bb30bc3c16859e70bc1fa9928a2048
Naomi <151114635+naomi-ho@users.noreply.github.com>
2024-02-17 12:09:31 +1100

    Implement function where clicking on a number updates the calculator display

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ let num2 = 0;
 let operator;
 let result = 0;
 
+const numbers = document.querySelectorAll(".number");
+const display = document.querySelector(".display");
+
 function add(num1, num2) {
     result = num1 + num2;
     console.log(`${result}`);
@@ -44,8 +47,22 @@ function operate(num1, num2, operator) {
     }
 }
 
-// test each operator
-// operate(1, 2, "+");
-// operate(1, 2, "-");
-// operate(1, 2, "*");
-// operate(1, 2, "/");
+// clicking on a button adds the number value to the display
+function populate() {
+    for (let i = 0; i < numbers.length; i++) {
+        let number = numbers[i];
+        number.addEventListener("click", () => {
+            display.textContent.length++;
+            console.log(display.textContent, display.textContent.length);
+            if (display.textContent.length > 11 && display.textContent.length < 18) {
+                display.style.fontSize = "200%";
+                display.style.paddingTop = "40px";
+            } else if (display.textContent.length >= 18) {
+                number.disabled = true;
+            }
+            return (display.textContent += number.textContent, display.textContent.length);
+        });
+    }
+}
+
+populate();git 
