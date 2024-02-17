7bca262c1efc9227f8875065512ea0720bc08050
Naomi <151114635+naomi-ho@users.noreply.github.com>
2024-02-17 23:17:03 +1100

    Assign display.textContent and display.textContent.length to variables to improve readability

--- script.js
+++ script.js
@@ -6,6 +6,27 @@ let result = 0;
 const numbers = document.querySelectorAll(".number");
 const display = document.querySelector(".display");
 
+// clicking on a button adds the number value to the display
+function populate() {
+    for (let i = 0; i < numbers.length; i++) {
+        let number = numbers[i];
+        number.addEventListener("click", () => {
+            display.textContent.length++;
+            if (display.textContent.length > 11 && display.textContent.length < 18) {
+                display.style.fontSize = "200%";
+                display.style.paddingTop = "40px";
+            } else if (display.textContent.length >= 18) {
+                number.disabled = true;
+            }
+            display.textContent += number.textContent;
+            let displayInput = display.textContent;
+            let displayLength = display.textContent.length;
+            console.log(displayInput, displayLength);
+            return (displayInput, displayLength);
+        });
+    }
+}
+
 function add(num1, num2) {
     result = num1 + num2;
     console.log(`${result}`);
@@ -47,22 +68,4 @@ function operate(num1, num2, operator) {
     }
 }
 
-// clicking on a button adds the number value to the display
-function populate() {
-    for (let i = 0; i < numbers.length; i++) {
-        let number = numbers[i];
-        number.addEventListener("click", () => {
-            display.textContent.length++;
-            console.log(display.textContent, display.textContent.length);
-            if (display.textContent.length > 11 && display.textContent.length < 18) {
-                display.style.fontSize = "200%";
-                display.style.paddingTop = "40px";
-            } else if (display.textContent.length >= 18) {
-                number.disabled = true;
-            }
-            return (display.textContent += number.textContent, display.textContent.length);
-        });
-    }
-}
-
-populate();git 
+populate();
