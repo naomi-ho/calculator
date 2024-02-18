a727bad3ab32e3805d0d2a040d89fe22c128bd59
Naomi <151114635+naomi-ho@users.noreply.github.com>
2024-02-18 20:39:17 +1100

    AC buttons results in reloadig the page and new variables display and displayLength added to improve readability

--- script.js
+++ script.js
@@ -1,42 +1,54 @@
 let num1 = 0;
 let num2 = 0;
 let operator;
+let equation = [];
 let result = 0;
+let num1Stored = false;
 
 const numbers = document.querySelectorAll(".number");
-const display = document.querySelector(".display");
+let display = document.querySelector(".display").textContent;
+let displayLength = document.querySelector(".display").textContent.length;
 const operators = document.querySelectorAll(".operator");
+const ac = document.querySelector("#ac");
+
+// clear calculator by reloading page
+function clear() {
+    ac.addEventListener("click", () => {
+    location.reload();
+    });
+}
 
 // clicking on a button adds the number value to the display
 function populate() {
     for (let i = 0; i < numbers.length; i++) {
         let number = numbers[i];
         number.addEventListener("click", () => {
-            display.textContent.length++;
-            if (display.textContent.length > 11 && display.textContent.length < 18) {
+            displayLength++;
+            if (displayLength > 11 && displayLength < 18) {
                 display.style.fontSize = "200%";
                 display.style.paddingTop = "40px";
-            } else if (display.textContent.length >= 18) {
+            } else if (displayLength >= 18) {
                 number.disabled = true;
             }
-            display.textContent += number.textContent;
-            let displayInput = display.textContent;
-            let displayLength = display.textContent.length;
-            console.log(displayInput, displayLength);
-            return (displayInput, displayLength);
+            display += number.textContent;
+            console.log(display, displayLength);
+            return (display, displayLength);
+
         });
     }
 }
 
-function stageOneStore() {
+function store() {
     for (let i = 0; i < operators.length; i++) {
         let operation = operators[i];
         let selectedOperator = false;
 
+
         // store first number and operator when user clicks an operator button
         operation.addEventListener("click", () => {
             selectedOperator = true;
-            num1 = display.textContent;
+            num1Stored = true;
+            num1 = display;
             operator = operation.textContent;
             console.log(num1, operator);
             return (num1, operator);
@@ -46,33 +58,41 @@ function stageOneStore() {
         if (selectedOperator === true) {
             operation.addEventListener("click", () => {
                 operation = "";
+                display.textContent = "";
             });
         }
     }
 }
 
+// function execute() {
+//     // clear display when clicking first digit of second number
+//     for (let i = 0; i < numbers.length; i++) {
+//         let number = numbers[i];
+//         number.addEventListener("click", () => {
+//             di
+//         });
+//     // run populate() again
+//     // run operate() when user clicks the equal button
+// }
+
 function add(num1, num2) {
-    result = num1 + num2;
     console.log(`${result}`);
-    return result;
+    return result = num1 + num2;
 }
 
 function subtract(num1, num2) {
-    result = num1 - num2;
     console.log(result);
-    return result;
+    return result = num1 - num2;
 }
 
 function multiply(num1, num2) {
-    result = num1 * num2;
     console.log(result);
-    return result;
+    return result = num1 * num2;
 }
 
 function divide(num1, num2) {
-    result = num1 / num2;
     console.log(result);
-    return result;
+    return result = num1 / num2;
 }
 
 function operate(num1, num2, operator) {
@@ -94,7 +114,9 @@ function operate(num1, num2, operator) {
 
 function calculate() {
     populate();
-    stageOneStore();
+    store();
+    clear();
+    // execute();
     // operate(num1, num2, operator);
 }
 
